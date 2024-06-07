c4ddea4784d2e13c37c9bb96b82a3bff847369ff
nKeys Kuo <nkeyskuo124@gmail.com>
2024-06-07 18:22:10 +0700

    forget configdev

--- frontend/src/configs/config.dev.js
+++ frontend/src/configs/config.dev.js
@@ -1,6 +1,6 @@
 export default {
-	API_URL: "http://localhost:2405/api/v1",
-	// API_URL: "https://ezticket.io.vn/api/v1",
+	// API_URL: "http://localhost:2405/api/v1",
+	API_URL: "https://ezticket.io.vn/api/v1",
 };
 
 
