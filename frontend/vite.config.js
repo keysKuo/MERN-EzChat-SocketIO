df667bce1206b424cb5ea842583ff084effd0117
nKeys Kuo <nkeyskuo124@gmail.com>
2024-06-08 11:44:03 +0700

    debug env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dotenv],
+  plugins: [react()],
+  define: {
+    'process.env.VITE_NODE_ENV': JSON.stringify(process.env.VITE_NODE_ENV),
+    'process.env.VITE_SOCKET_URL': JSON.stringify(process.env.VITE_SOCKET_URL),
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
+  }
 })
